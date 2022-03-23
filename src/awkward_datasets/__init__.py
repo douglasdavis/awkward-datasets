0d7cf98a1f9a7c7589745fd412ecf7be75133fe9
Doug Davis <ddavis@ddavis.io>
2022-03-23 13:23:55 -0400

    pre-commit

--- src/awkward_datasets/__init__.py
+++ src/awkward_datasets/__init__.py
@@ -2,9 +2,9 @@ from awkward_datasets._version import version
 from awkward_datasets.json import (
     line_delimited_records,
     line_delimited_records_text,
+    muon_files,
     single_record,
     single_record_text,
-    muon_files,
 )
 
 __version__ = version
