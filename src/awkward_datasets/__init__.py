50a53de732c885bf441cd2bf412b77936e61de39
Doug Davis <ddavis@anaconda.com>
2022-03-23 12:56:12 -0400

    organization

--- src/awkward_datasets/__init__.py
+++ src/awkward_datasets/__init__.py
@@ -1,3 +1,18 @@
 from awkward_datasets._version import version
+from awkward_datasets.json import (
+    line_delimited_records,
+    line_delimited_records_text,
+    single_record,
+    single_record_text,
+    muon_files,
+)
 
 __version__ = version
+
+__all__ = (
+    "line_delimited_records",
+    "line_delimited_records_text",
+    "single_record",
+    "single_record_text",
+    "muon_files",
+)
