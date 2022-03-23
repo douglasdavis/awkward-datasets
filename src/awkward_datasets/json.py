086ced2a42dd657309ca4986236e2232a8e373ea
Doug Davis <ddavis@anaconda.com>
2022-03-23 12:44:38 -0400

    muons; organization

--- src/awkward_datasets/json.py
+++ src/awkward_datasets/json.py
@@ -5,13 +5,13 @@ from pathlib import Path
 DIR = Path(__file__).resolve()
 
 
-def line_delimited_records() -> str:
-    f = DIR / "data" / "line_delim_records.json"
+def line_delimited_records_text() -> str:
+    f = DIR / "data" / "simple" / "line_delim_records.json"
     return f.read_text()
 
 
-def single_record() -> str:
-    f = DIR / "data" / "single_record.json"
+def single_record_text() -> str:
+    f = DIR / "data" / "simple" / "single_record.json"
     return f.read_text()
 
 
